9c4192316b90331b124d76cbc3254c96a130aa53
zahidsandhu <zahidsandhu77a@gmail.com>
2021-06-17 12:34:06 +0500

    new 3 delete

--- userpanel/apps/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// $con = mysqli_connect("localhost","deveruxr_prizebond","deveruxr_prizebond","deveruxr_prizebond");
-$con = mysqli_connect("localhost","deveruxr_prizebond","deveruxr_prizebond","deveruxr_prizebond");
-if (!$con)
-{
- 	die('Could not connect: ' . mysqli_error());
-}
-?>
